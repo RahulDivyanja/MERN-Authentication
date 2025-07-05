ce05f9936e679c13969863ce40f8c324824bb068
Rahul Divyanja <rahuldivyanja@gmail.com>
2025-07-05 14:42:16 +0530

    Index.js edited for deploy

--- backend/index.js
+++ backend/index.js
@@ -16,13 +16,13 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/user", authRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+//   });
+// }
 
 (async () => {
   try {
